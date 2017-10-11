881bcea24317e83da5684e255a74231637c499be
jose <japv1992@gmail.com>
2017-10-11 02:59:41 -0600

    ejercicio suma corregido

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,19 @@ res.render('suma',{});
 });
 router.get('/0801199219353',function(req,res,next){
   res.json(misDatos);
-//  res.render('0801199219353',{})
+
 });
 router.post('/suma',function(req,res,next){
   console.log(req.body);
   var newSuma = Object.assign({},resultado);
   newSuma.numero1 = req.body.txtN1;
   newSuma.numero2 = req.body.txtN2;
-
+  newSuma.resultado = parseInt(req.body.txtN1)+parseInt(req.body.txtN2);
+  resultado.push(newSuma);
+  console.log(resultado);
+  var data =Object.assign({}, req.body);
+  data.resultado = resultado;
+  res.render('suma',data);
 });
 
 module.exports = router;
